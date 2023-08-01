3e835f4e92d652308fa34296e0e437527f87f59c
Anaks-CT <anaksthayyil30@gmail.com>
2023-08-01 14:28:23 +0530

    added error handling middleware

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ import dotenv from 'dotenv'
 import connectDB from './conifg/database.js';
 import express, { urlencoded } from 'express';
 import user from './routes/user.js';
+import notFound from './middleware/urlNotFound.js';
+import errorHandler from './middleware/errorHandler.js';
 
 
 dotenv.config();
@@ -13,7 +15,15 @@ connectDB();
 const app = express();
 
 app.use(express.json()); // to accept json data
+
+// route
 app.use('/api/user', user)
+
+
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
+
 
 const PORT = process.env.PORT;
 
