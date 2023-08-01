02eb08d316b003f03ed1b83cb83b50868ee5fe69
Anaks-CT <anaksthayyil30@gmail.com>
2023-08-01 15:31:41 +0530

    implimented morgan for logger. configured cors for the frontend entry

--- server.js
+++ server.js
@@ -8,13 +8,23 @@ import express, { urlencoded } from 'express';
 import user from './routes/user.js';
 import notFound from './middleware/urlNotFound.js';
 import errorHandler from './middleware/errorHandler.js';
-
+import cors from 'cors'
+import morgan from 'morgan';
 
 dotenv.config();
 connectDB();
 const app = express();
-
+app.use(
+  cors({
+    origin: [process.env.ORIGIN_URL],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(morgan('combined'));
 app.use(express.json()); // to accept json data
+
+// cors configuration
 
 // route
 app.use('/api/user', user)
